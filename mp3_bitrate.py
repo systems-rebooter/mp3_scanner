507a8dac3a119334a03919a92282683806096ceb
tatico <tatico@gmail.com>
2012-09-09 14:08:05 +0200

    adding local files

---
+++ mp3_bitrate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# mp3_bitrate.py 
+
+import os, sys, hashlib, fnmatch
+
+from mutagen.mp3 import MP3
+
+
+# set to false for quiet output
+debug = True 
+#debug = False 
+
+
+# TODO
+# store info in sqlite or so 
+# by now, > to a file does it.
+
+if len(sys.argv) > 1:
+  path=sys.argv[1]
+else:
+  print("usage: %s /path/to/mp3s/") %sys.argv[0]
+  sys.exit(1)
+
+def indexer(path):
+  if os.path.isdir(path):
+    filesindir= os.listdir(path)
+
+    for directory, subdirs, filenames in os.walk(path):
+      if filenames != []:
+        for filename in filenames:
+          if fnmatch.fnmatch(filename, '*.mp3'):
+            current_file_name = os.path.join(directory,filename)
+  #          print current_file_name
+            f = open(current_file_name, 'rb')
+            h = hashlib.md5()
+            h.update(f.read())
+            md5sum = h.hexdigest()
+            f.close()
+            input_file = MP3(current_file_name)
+            raw_bitrate = input_file.info.bitrate
+            bitrate = (raw_bitrate / 1000)
+            if debug == True:
+              print("%s; %s; %s") %(current_file_name, bitrate, md5sum)
+      
+  else:
+    print("%s is file") %path
+
+
+if __name__ == "__main__":
+  indexer(path)
