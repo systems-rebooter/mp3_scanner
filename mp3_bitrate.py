09cf25fd53de6bc81891f68225979fa81dd4b037
f varas <fvaras@smartjog.com>
2012-09-15 15:09:02 +0200

    added test to survive corrupted mp3s

--- mp3_bitrate.py
+++ mp3_bitrate.py
@@ -4,12 +4,16 @@
 
 import os, sys, hashlib, fnmatch
 
-from mutagen.mp3 import MP3
-
+try:
+  from mutagen.mp3 import MP3
+  import mutagen
+except ImportError:
+  print("install mutagen!!\non Debian like distributions, you can do: apt-get install python-mutagen")
+  sys.exit(1)
 
 # set to false for quiet output
-debug = True 
-#debug = False 
+#debug = True 
+debug = False 
 
 
 # TODO
@@ -31,17 +35,20 @@ def indexer(path):
         for filename in filenames:
           if fnmatch.fnmatch(filename, '*.mp3'):
             current_file_name = os.path.join(directory,filename)
-  #          print current_file_name
+            print current_file_name
             f = open(current_file_name, 'rb')
             h = hashlib.md5()
             h.update(f.read())
             md5sum = h.hexdigest()
             f.close()
-            input_file = MP3(current_file_name)
-            raw_bitrate = input_file.info.bitrate
-            bitrate = (raw_bitrate / 1000)
-            if debug == True:
-              print("%s; %s; %s") %(current_file_name, bitrate, md5sum)
+            try:
+              input_file = MP3(current_file_name)
+              raw_bitrate = input_file.info.bitrate
+              bitrate = (raw_bitrate / 1000)
+              if debug == True:
+               print("%s; %s; %s") %(current_file_name, bitrate, md5sum)
+            except mutagen.mp3.HeaderNotFoundError:
+              print("corrupted mp3")
       
   else:
     print("%s is file") %path
